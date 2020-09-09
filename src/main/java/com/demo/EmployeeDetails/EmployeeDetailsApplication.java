fe2577ff233a4dab3d8da26416b0b7d080006bc2
SanaMaya <66974650+SanaMaya@users.noreply.github.com>
2020-09-09 19:17:40 -0400

    Update EmployeeDetailsApplication.java

--- src/main/java/com/demo/EmployeeDetails/EmployeeDetailsApplication.java
+++ src/main/java/com/demo/EmployeeDetails/EmployeeDetailsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeDetailsApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeDetailsApplication.class, args);
 	}
